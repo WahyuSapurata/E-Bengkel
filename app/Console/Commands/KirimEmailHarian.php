3ce4a53d0f3f3a83a21366707c750216ffffb5de
WahyuSapurata <wm337708@gmail.com>
2025-10-17 08:22:30 +0800

    update KirimEmailHarian

--- app/Console/Commands/KirimEmailHarian.php
+++ app/Console/Commands/KirimEmailHarian.php
@@ -28,8 +28,7 @@ class KirimEmailHarian extends Command
      */
     public function handle()
     {
-        // $tanggalHariIni = Carbon::now()->format('d-m-Y');
-        $tanggalHariIni = "16-10-2025";
+        $tanggalHariIni = Carbon::now()->format('d-m-Y');
 
         // Ambil semua outlet
         $outlets = Outlet::all();
